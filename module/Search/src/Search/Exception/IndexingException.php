4fef3239c6296db386edadf227e9917e73ea1add
Haydar KULEKCI <haydarkulekci@gmail.com>
2015-12-09 21:41:38 +0200

    Added IndexingException and update MissingDataException comment Added partial update Added delete method to delete document by id

---
+++ module/Search/src/Search/Exception/IndexingException.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Indexing Exception
+ *
+ * @since     Dec 2015
+ * @author    Haydar KULEKCI <haydarkulekci@gmail.com>
+ */
+namespace Search\Exception;
+
+class IndexingException extends \Exception
+{
+}
--- module/Search/src/Search/Index/Service/AbstractIndexService.php
+++ module/Search/src/Search/Index/Service/AbstractIndexService.php
@@ -7,15 +7,21 @@
  */
 namespace Search\Index\Service;
 
+use Elastica\Document;
+use Search\Exception\IndexingException;
 use Search\Exception\MissingDataException;
 use Search\Service\AbstractSearchService;
 
 class AbstractIndexService extends AbstractSearchService
 {
     /**
+     * Indexing Data
      *
      * @param  array    $data
      * @param  integer  $version
+     *
+     * @throws IndexingException
+     *
      * @return null
      */
     public function index(array $data, $version = 1)
@@ -25,7 +31,54 @@ class AbstractIndexService extends AbstractSearchService
         }
 
         //TODO: this creation will be moved to a builder
-        $document = new \Elastica\Document($data['id'], $data);
-        $this->getType($version)->addDocument($document);
+        $document = new Document($data['id'], $data);
+        try {
+            $this->getType($version)->addDocument($document);
+        } catch (\Exception $e) {
+            throw new IndexingException('Throw exception while indexing', $e->getCode(), $e);
+        }
+    }
+
+    /**
+     * Partial Update
+     *
+     * @param  string  $docId
+     * @param  array    $data
+     * @param  integer  $version
+     *
+     * @throws IndexingException
+     *
+     * @return null
+     */
+    public function update($docId, array $data, $version = 1)
+    {
+        $document = new Document();
+        $document->setData($data);
+        $document->setId($docId);
+        $document->setDocAsUpsert(true);
+        try {
+            $this->getType($version)->updateDocument($document);
+        } catch (\Exception $e) {
+            throw new IndexingException('Throw exception while updating', $e->getCode(), $e);
+        }
+    }
+
+    /**
+     * Partial Update
+     *
+     * @param  string  $docId
+     * @param  integer  $version
+     *
+     * @throws IndexingException
+     *
+     * @return null
+     */
+    public function delete($docId, $version = 1)
+    {
+        try {
+            $this->getType($version)->delete($docId);
+        } catch (\Exception $e) {
+            throw new IndexingException('Throw exception while updating', $e->getCode(), $e);
+        }
     }
 }
